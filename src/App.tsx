a36ca3d8760e92ad70419d13e850820c540490f8
Holly Lampert <holly.lampert@dunelm.com>
2024-07-26 11:14:57 +0100

    Added log out feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/clerk-react";
 
 
 function App() {
@@ -34,6 +34,7 @@ function App() {
         <p className="read-the-docs">
           Click on the Vite and React logos to learn more
         </p>
+        <SignOutButton />
       </SignedIn>
     </>
   )
