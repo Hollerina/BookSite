24cc66e218ac54ad843d7e9189adc03bc5099151
Holly Lampert <holly.lampert@dunelm.com>
2024-08-01 14:30:59 +0100

    Added Navbar using mantine

--- src/pages/currently-reading-page/CurrentlyReadingPage.tsx
+++ src/pages/currently-reading-page/CurrentlyReadingPage.tsx
@@ -1,6 +1,9 @@
+import NavBar from "../components/NavBar";
+
 const CurrentlyReadingPage = () => {
   return (
     <>
+      <NavBar/>
       <h1>Currently Reading Page</h1>
     </>
   );
--- src/pages/profile-page/index.tsx
+++ src/pages/profile-page/index.tsx
@@ -1,3 +1,3 @@
-import ProfilePage from './ProfilePage.tsx'
+import ProfilePage from './ProfilePage'
 
 export default ProfilePage
