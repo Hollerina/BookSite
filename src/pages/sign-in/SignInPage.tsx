f5c1590b74dd368cf5c712065f8e8703dea7ac91
Holly Lampert <holly.lampert@dunelm.com>
2024-08-01 11:13:56 +0100

    Updated sign in and sign up pages to match format of others

--- src/pages/sign-in/SignInPage.tsx
+++ src/pages/sign-in/SignInPage.tsx
@@ -1,5 +1,7 @@
 import { SignIn } from "@clerk/clerk-react";
 
-export default function SignInPage() {
+const SignInPage = () => {
   return <SignIn path="/sign-in" />;
-}
+};
+
+export default SignInPage;
