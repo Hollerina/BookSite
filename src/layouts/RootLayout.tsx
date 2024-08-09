1b75a1d139c1ea1b135ce5003aa873ea9f9f1b45
Holly Lampert <holly.lampert@dunelm.com>
2024-08-09 10:38:17 +0100

    Removed unused component

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,7 +1,6 @@
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { ClerkProvider, SignedOut } from "@clerk/clerk-react";
 import NavBar from "../pages/components/NavBar";
-import styled from "styled-components";
 
 const PUBLISHABLE_KEY =
   "pk_test_YmVjb21pbmctYmx1ZWdpbGwtNjEuY2xlcmsuYWNjb3VudHMuZGV2JA";
@@ -12,10 +11,6 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
-
-const Header = styled.header``;
-
-
 export default function RootLayout() {
   const navigate = useNavigate();
 
@@ -25,12 +20,12 @@ export default function RootLayout() {
       routerReplace={(to) => navigate(to, { replace: true })}
       publishableKey={PUBLISHABLE_KEY}
     >
-      <Header>
+      <header>
         <NavBar />
         <SignedOut>
           <Link to="/sign-in">Sign In</Link>
         </SignedOut>
-      </Header>
+      </header>
 
       <main>
         <Outlet />
