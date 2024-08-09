19bd6c0a126aca1215da7f209a84c2166722d883
Holly Lampert <holly.lampert@dunelm.com>
2024-08-09 10:20:09 +0100

    Navbar has tabbing

--- src/pages/components/NavBar.tsx
+++ src/pages/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { SignedIn, UserButton } from "@clerk/clerk-react";
-import { Anchor } from "@mantine/core";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 
 const NavBarContainer = styled.div`
@@ -14,28 +14,70 @@ const NavBarContainer = styled.div`
 
 const AnchorContainer = styled.div`
   display: flex;
+`;
+
+const ActiveAnchor = styled.a`
+  background-color: white;
+  color: black;
+  padding: 20px;
+  margin-left: 10px;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  border-left: black solid 1px;
+  border-right: black solid 1px;
+  border-top: black solid 1px;
+  border-bottom-color: transparent;
   margin-bottom: -1px;
+  text-decoration: none;
+`;
+
+const InactiveAnchor = styled.a`
+  background-color: green;
+  color: black;
+  padding: 20px;
+  margin-left: 10px;
+  border-bottom-color: transparent;
+  margin-bottom: -1px;
+  text-decoration: none;
 `;
 
 const ProfileContainer = styled.div``;
 
 const Navbar = () => {
+  const location = useLocation();
+  console.log(location.pathname);
+  console.log(location.pathname === "/profile");
+
   return (
     <NavBarContainer>
       <AnchorContainer>
-        <Anchor href="/profile" target="_self" underline="never">
-          Profile
-        </Anchor>
-        <Anchor
-          href="/profile/currently-reading"
-          target="_self"
-          underline="never"
-        >
-          Currently reading
-        </Anchor>
-        <Anchor href="/profile/read-books" target="_self" underline="never">
-          Read books
-        </Anchor>
+        {location.pathname === "/profile" ? (
+          <ActiveAnchor href="/profile" target="_self">
+            Profile
+          </ActiveAnchor>
+        ) : (
+          <InactiveAnchor href="/profile" target="_self">
+            Profile
+          </InactiveAnchor>
+        )}
+        {location.pathname === "/profile/currently-reading" ? (
+          <ActiveAnchor href="/profile/currently-reading" target="_self">
+            Currently Reading
+          </ActiveAnchor>
+        ) : (
+          <InactiveAnchor href="/profile/currently-reading" target="_self">
+            Currently Reading
+          </InactiveAnchor>
+        )}
+        {location.pathname === "/profile/read-books" ? (
+          <ActiveAnchor href="/profile/read-books" target="_self">
+            Read Books
+          </ActiveAnchor>
+        ) : (
+          <InactiveAnchor href="/profile/read-books" target="_self">
+            Read Books
+          </InactiveAnchor>
+        )}
       </AnchorContainer>
       <ProfileContainer>
         <SignedIn>
