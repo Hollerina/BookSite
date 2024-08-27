b9091778499fc228b88845dc17ef6ad625231476
Holly Lampert <holly.lampert123@gmail.com>
2024-08-27 14:42:47 +0100

    removed semicolons

--- src/pages/components/__tests__/NavBar.test.tsx
+++ src/pages/components/__tests__/NavBar.test.tsx
@@ -1,9 +1,9 @@
-import { fireEvent, render } from "@testing-library/react";
-import NavBar from "../NavBar";
-import "@testing-library/jest-dom";
-import { MemoryRouter } from "react-router-dom";
-import { ClerkProvider } from "@clerk/clerk-react";
-import { screen } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react"
+import NavBar from "../NavBar"
+import "@testing-library/jest-dom"
+import { MemoryRouter } from "react-router-dom"
+import { ClerkProvider } from "@clerk/clerk-react"
+import { screen } from "@testing-library/react"
 
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -18,35 +18,35 @@ const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
         {children}
       </ClerkProvider>
     </MemoryRouter>
-  );
-};
+  )
+}
 
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
   useLocation: () => ({
     pathname: "/profile",
   }),
-}));
+}))
 
 describe("Navbar", () => {
   //Snapshot doesn't render with CSS
   it("should match the snapshot", () => {
-    const { container } = render(<NavBar />, { wrapper: AllTheProviders });
+    const { container } = render(<NavBar />, { wrapper: AllTheProviders })
 
-    expect(container).toMatchSnapshot();
-  });
+    expect(container).toMatchSnapshot()
+  })
   it.each([
     ["/profile", "Profile"],
     ["/profile/currently-reading", "Currently Reading"],
     ["/profile/read-books", "Read Books"],
   ])("should have a href %s when %s is clicked", (hrefLink, navbarTitle) => {
-    render(<NavBar />, { wrapper: AllTheProviders });
-    const link = screen.getByText(navbarTitle);
-    fireEvent.click(link);
+    render(<NavBar />, { wrapper: AllTheProviders })
+    const link = screen.getByText(navbarTitle)
+    fireEvent.click(link)
 
     expect(screen.getByRole("link", { name: navbarTitle })).toHaveAttribute(
       "href",
       hrefLink
-    );
-  });
-});
+    )
+  })
+})
