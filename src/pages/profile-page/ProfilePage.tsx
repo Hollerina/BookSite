d0a1df59b5f1db1a4bfbf8104d09d32381aa7618
Holly Lampert <holly.lampert123@gmail.com>
2024-08-29 16:08:22 +0100

    the layout for profile page

--- src/pages/profile-page/ProfilePage.tsx
+++ src/pages/profile-page/ProfilePage.tsx
@@ -1,9 +1,69 @@
+import { useUser } from "@clerk/clerk-react"
+import styled from "@emotion/styled"
+
+const ProfilePageContainer = styled.div`
+  display: grid;
+  max-width: 1284px;
+  padding: 24px 16px;
+  margin: auto;
+  gap: 10px 20px;
+  justify-items: center;
+
+  grid-template-areas:
+    "profile-image profile-details profile-details profile-details"
+    "favourites favourites favourites favourites"
+    "books-owned books-owned want-to-read want-to-read"
+    "to-be-read to-be-read other other";
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+`
+
+// "books-owned" "books-owned" "want-to-read" "want-to-read"
+//     "to-be-read" "to-be-read" "other" "other";
+
+const ProfilePicture = styled.img`
+  grid-area: profile-image;
+`
+
+const ProfileDetailsContainer = styled.div`
+  grid-area: profile-details;
+  height: fit-content;
+`
+const FavouritesContainer = styled.div`
+  grid-area: favourites;
+`
+
+const BooksOwnedContainer = styled.div`
+  grid-area: books-owned;
+`
+
+const WantToReadContainer = styled.div`
+  grid-area: want-to-read;
+`
+
+const ToBeReadContainer = styled.div`
+  grid-area: to-be-read;
+`
+
+const OtherContainer = styled.div`
+  grid-area: other;
+`
+
 const ProfilePage = () => {
-    return (
-      <>
-        <h1>Profile Page</h1>
-      </>
-    );
-  };
-  
-  export default ProfilePage;
+  const { isLoaded, isSignedIn, user } = useUser()
+
+  console.log(user?.username)
+
+  return (
+    <ProfilePageContainer>
+      <ProfilePicture src={user?.imageUrl} />
+      <ProfileDetailsContainer>heading</ProfileDetailsContainer>
+      <FavouritesContainer> here</FavouritesContainer>
+      <BooksOwnedContainer>Books I own</BooksOwnedContainer>
+      <WantToReadContainer>Want to read</WantToReadContainer>
+      <ToBeReadContainer>To be read</ToBeReadContainer>
+      <OtherContainer>Other</OtherContainer>
+    </ProfilePageContainer>
+  )
+}
+
+export default ProfilePage
